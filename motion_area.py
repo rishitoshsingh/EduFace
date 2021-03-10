8b63cbe7243323542895df32e5878a4f6a371237
rishitoshsingh <rishitoshs@gmail.com>
2021-03-10 13:11:17 +0530

    added motion area selector

---
+++ motion_area.py
@@ -0,0 +1,69 @@
+import cv2
+from imutils.video import VideoStream
+import numpy as np
+import json
+from models import Camera
+
+
+with open('cameras.json','r') as file:
+    cameras_dicts = json.load(file)
+
+UPDATE_NUM = 1
+RESOLUTION = (1280, 720)
+
+cameras = []
+for camera_d in cameras_dicts:
+    c = Camera(**camera_d)
+    cameras.append(c)
+
+motion_boxes = {}
+
+for camera in cameras:
+    cap = VideoStream(camera.get_stream()).start()
+    try:
+        frame = cap.read()
+        frame = cv2.resize(frame, RESOLUTION)
+        (height, width) = frame.shape[:2]
+        motion_box = {
+            'corner_1': [0,0],
+            'corner_2': [width, height]
+        }
+        while True:
+            frame = cap.read()
+            frame = cv2.resize(frame, RESOLUTION)
+            
+            frame = cv2.putText(frame, str(camera), (0, 50), cv2.FONT_HERSHEY_PLAIN, 2, (225,0,0),2)
+            frame = cv2.putText(frame, str(motion_box['corner_1']), (0, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0,225,0),2)
+            frame = cv2.putText(frame, str(motion_box['corner_2']), (0, 200), cv2.FONT_HERSHEY_PLAIN, 2, (0,225,0),2)
+            
+            frame = cv2.rectangle(frame, tuple(motion_box['corner_1']), tuple(motion_box['corner_2']), (225,0,0),2)
+            cv2.imshow(str(camera), frame)
+            key = cv2.waitKey(1)
+            if key == ord('w'):
+                motion_box['corner_1'][1] = (motion_box['corner_1'][1] - UPDATE_NUM) % height
+            elif key == ord('a'):
+                motion_box['corner_1'][0] = (motion_box['corner_1'][0] - UPDATE_NUM) % width
+            elif key == ord('s'):
+                motion_box['corner_1'][1] = (motion_box['corner_1'][1] + UPDATE_NUM) % height
+            elif key == ord('d'):
+                motion_box['corner_1'][0] = (motion_box['corner_1'][0] + UPDATE_NUM) % width
+            
+            elif key == ord('i'):
+                motion_box['corner_2'][1] = (motion_box['corner_2'][1] - UPDATE_NUM) % height
+            elif key == ord('j'):
+                motion_box['corner_2'][0] = (motion_box['corner_2'][0] - UPDATE_NUM) % width
+            elif key == ord('k'):
+                motion_box['corner_2'][1] = (motion_box['corner_2'][1] + UPDATE_NUM) % height
+            elif key == ord('l'):
+                motion_box['corner_2'][0] = (motion_box['corner_2'][0] + UPDATE_NUM) % width
+            elif key == 27:
+                motion_boxes.update({camera.get_id(): motion_box})
+                break
+    except KeyboardInterrupt:
+        pass
+    finally:
+        cap.stop()
+        cv2.destroyAllWindows()
+
+with open('motion_boxes_temp.json','w') as file:
+    json.dump(motion_boxes, file, indent=4)
--- pipeline/producer.py
+++ pipeline/producer.py
@@ -41,7 +41,7 @@ class StreamCamera(multiprocessing.Process):
         # self.Gthread = Thread(target=self.main_loop.run)
         # self.Gthread.start()
         # self.pipeline = Gst.parse_launch("rtspsrc location={} ! autovideosink".format(self.camera.get_stream()))
-        pipeline_str = "uridecodebin uri={} uridecodebin0::source::latency=300 ! videoscale ! video/x-raw,width=1080,height=720 ! videoconvert !  video/x-raw, format=RGB ! appsink name={}".format(self.camera.get_stream(), self.camera.get_id())
+        pipeline_str = "uridecodebin uri={} uridecodebin0::source::latency=300 ! videoscale ! video/x-raw,width=1280,height=720 ! videoconvert !  video/x-raw, format=RGB ! appsink name={}".format(self.camera.get_stream(), self.camera.get_id())
         # pipeline_str = "rtspsrc location={} ! queue ! rtph265depay ! h265parse ! avdec_h265 ! videoconvert !  video/x-raw, format=RGB ! appsink name={}".format(self.camera.get_stream(), self.camera.get_id())
         self.pipeline = Gst.parse_launch(pipeline_str)
         self.appsink = self.pipeline.get_by_name("{}".format(self.camera.get_id()))
